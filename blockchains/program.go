481cda820873d78145dbdac4e3f37d052085af95
Nazar <85495047+cryptomanz@users.noreply.github.com>
2022-02-18 09:46:15 +0800

    Create program.go

---
+++ blockchains/program.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+    "encoding/json"
+    "fmt"
+    "log"
+    "os"
+)
+
+type Student struct {
+    Name     string
+    
+}
+
+func main() {
+    // open the file pointer
+    studentFile, err := os.Open("data.json")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer studentFile.Close()
+
+    // create a new decoder
+    var studentDecoder *json.Decoder = json.NewDecoder(studentFile)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    // initialize the storage for the decoded data
+    var studentList []Student
+    
+    // decode the data
+    err = studentDecoder.Decode(&studentList)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    for i, student := range studentList {
+        fmt.Println("Student", i+1)
+        fmt.Println("Student name:", student.Name)
+    }
+}
