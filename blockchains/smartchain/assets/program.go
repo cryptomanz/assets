0fe9bf7737fbfd78b708647bfeee07c577877b41
Nazar <85495047+cryptomanz@users.noreply.github.com>
2022-02-18 15:06:36 +0800

    Update program.go

--- blockchains/smartchain/assets/program.go
+++ blockchains/smartchain/assets/program.go
@@ -1,32 +1,20 @@
+
 package main
 
 import (
-    "encoding/json"
     "fmt"
-    "log"
-    "os"
+    "io/ioutil"
 )
-type Symbol struct {
-    Symbol   string
-    
-}
+
 func main() {
-    symbolFile, err := os.Open("info.json")
+    files, err := ioutil.ReadDir(".")
     if err != nil {
-        log.Fatal(err)
+        panic(err)
     }
-    defer symbolFile.Close()
-    var studentDecoder *json.Decoder = json.NewDecoder(symbolFile)
-    if err != nil {
-        log.Fatal(err)
-    }
-    var studentList []Symbol
-    err = studentDecoder.Decode(&studentList)
-    if err != nil {
-        log.Fatal(err)
-    }
-    for i, student := range studentList {
-        fmt.Println("Symbol", i+1)
-        fmt.Println("Symbol:", student.Symbol)
+
+    fmt.Println("Files and folders in the current directory:")
+
+    for _, fileInfo := range files {
+        fmt.Println(fileInfo.Name())
     }
 }
