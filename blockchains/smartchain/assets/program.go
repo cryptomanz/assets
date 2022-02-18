c1070aab70fa0324ff06eda2c4f4c1dcd3fbd741
Nazar <85495047+cryptomanz@users.noreply.github.com>
2022-02-18 14:43:54 +0800

    Update program.go

--- blockchains/smartchain/assets/program.go
+++ blockchains/smartchain/assets/program.go
@@ -6,38 +6,27 @@ import (
     "log"
     "os"
 )
-
 type Symbol struct {
     Symbol   string
     
 }
-
 func main() {
-    // open the file pointer
     symbolFile, err := os.Open("info.json")
     if err != nil {
         log.Fatal(err)
     }
     defer symbolFile.Close()
-
-    // create a new decoder
     var studentDecoder *json.Decoder = json.NewDecoder(symbolFile)
     if err != nil {
         log.Fatal(err)
     }
-
-    // initialize the storage for the decoded data
     var studentList []Symbol
-    
-    // decode the data
     err = studentDecoder.Decode(&studentList)
     if err != nil {
         log.Fatal(err)
     }
-
     for i, student := range studentList {
         fmt.Println("Symbol", i+1)
         fmt.Println("Symbol:", student.Symbol)
-    
     }
 }
